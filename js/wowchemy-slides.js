96bb2f5eb8c8a8babe27d043727cc6512884fac0
Benjamin Hills <benjaminhhills@gmail.com>
2022-06-13 15:44:42 -0600

    Update with Siple Coast pub

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -3,7 +3,7 @@
   var slides = { highlight_style: "dracula", theme: "black" };
 
   // <stdin>
-  var enabledPlugins = [RevealMarkdown, RevealHighlight, RevealSearch, RevealNotes, RevealMath.MathJax3, RevealZoom];
+  var enabledPlugins = [RevealMarkdown, RevealSearch, RevealNotes, RevealMath.MathJax3, RevealZoom];
   var isObject = function(o) {
     return o === Object(o) && !isArray(o) && typeof o !== "function";
   };
